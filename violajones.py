635910f979f3c5f6052251205ec5202263942369
camu-izidoro <camila.izidoro@usp.br>
2019-06-09 15:11:26 -0300

    commit_violajones_0906

--- violajones.py
+++ violajones.py
@@ -4,22 +4,36 @@ import cv2  as cv
 import  matplotlib.pyplot as plt
 import os
 
-
 faceCascade = cv.CascadeClassifier('haarcascades\haarcascade_frontalface_default.xml')
 # Read image from your local file system
-# fazer um foreach pelas imagens
-original_image = cv.imread('eu_linkedin.jpg',0)
 
-# Detect faces
-faces = faceCascade.detectMultiScale(
-original_image,
-scaleFactor=1.1,
-minNeighbors=5,
-flags=cv.CASCADE_SCALE_IMAGE
-)
-# For each face
-for (x, y, w, h) in faces: 
-    # Draw rectangle around the face
-    sub_face = original_image[y:y+h, x:x+w]
-    cv.imwrite("crop1.jpg",sub_face)
+for i in range(1,51):
+    
+    # ele vai criar uma pasta e vai entrar em cada imagem e cortar
+    if len(str(i)) == 1:
+        numero_pasta="00" + str(i)
+    if len(str(i)) == 2:
+        numero_pasta="0" + str(i)
+    
+   
+    os.mkdir('./'+numero_pasta)
+    for v in range (1,6):
+        #formatar o numero
+       
+        pasta='C:/Users/camil/Desktop/Face Database part1/'+numero_pasta+'/'+numero_pasta+'_expresion_frown_'+str(v)+'.bmp'
+        original_image = cv.imread(pasta,0)
+        original_image_notgray = cv.imread(pasta,1)
+
+        # Detect faces
+        faces = faceCascade.detectMultiScale(
+        original_image,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        flags=cv.CASCADE_SCALE_IMAGE
+        )
+        # For each face
+        for (x, y, w, h) in faces: 
+            # Draw rectangle around the face
+            sub_face = original_image_notgray[y:y+h, x:x+w]
+            cv.imwrite('./'+numero_pasta+'/'+numero_pasta+'_crop_'+str(v)+'.bmp',sub_face)
 
